4910b34c4dfa6e20ddfc1d3d831b41308b2892ab
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-09 11:44:44 +0300

    Create 3.c

---
+++ Problems Sheet # 3: Basic Programming Concepts in C/Question 4/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    float length, width, area;
+    scanf("%f %f", &length, &width);
+  
+    area = length * width;
+    printf("Area: %.2f\n", area);
+    
+    return 0;
+}
