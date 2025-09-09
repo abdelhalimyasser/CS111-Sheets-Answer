4700d0e18078ae07265ca2ca98fc0d225ebde746
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-09 12:42:53 +0300

    Create 12.c

---
+++ Problems Sheet # 6: Functions & Recursion using C/Question 1/12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void printMenu() {
+    printf("Calculator Menu:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n0. Exit\n");
+}
+
+int main() {
+    printMenu();
+    return 0;
+}
